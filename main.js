b2eda857cfda05ee6b7f411783dd1636fbd79ba9
Deonte H (Sasko) <deonte@news-MacBook-Pro-2.local>
2022-03-29 13:09:31 -0700

    Second Uodate

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 // write your code here
 
-
-
+$.getJSON("./data.json", data => {
+    for(i= data.length -1; i >= 0; i--){
+        let nameColumn= `<tr><td>${data[i].name}</td><td>${data[i].description}</td> <td>${data[i].location}</td></tr>`
+        $("#topsTable").after(nameColumn);
+    }
+})
 });
-.getJSON
+
 
