450f193c97f0a148e0989b5e0b54324691dd6692
Deonte H (Sasko) <deonte@news-MacBook-Pro-2.local>
2022-03-29 15:15:44 -0700

    broken but passes tests, i everything is accurate, just in wrong place

--- main.js
+++ main.js
@@ -2,9 +2,28 @@ $(document).ready(function() {
 // write your code here
 
 $.getJSON("./data.json", data => {
-    for(i= data.length -1; i >= 0; i--){
+    
+   let mapLinks=[
+       "https://goo.gl/maps/ad25gQFtC3mrD7s16" ,
+         "https://goo.gl/maps/kcMujEmr96asT2Cr9" ,    
+        "https://goo.gl/maps/G2XLw6wdTWjKajEA9" ,    
+        "https://goo.gl/maps/VRejzGFqFTUeNd8e6"    ]
+    data = data.reverse();
+    for(i= 0; i < data.length; i++){
+        var a = document.createElement('a'); 
+        var link = document.createTextNode("Link to Google Maps");
+        a.appendChild(link);
+        a.title = "Link to Google Maps";
+       
+        a.href= mapLinks[i];
+        document.body.appendChild(a);
+
+
+
         let nameColumn= `<tr><td>${data[i].name}</td><td>${data[i].description}</td> <td>${data[i].location}</td></tr>`
         $("#topsTable").after(nameColumn);
+        console.log(data[i].location[0])
+
     }
 })
 });
